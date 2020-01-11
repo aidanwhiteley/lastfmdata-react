2d3f33615abc32620f4165a673eb231e1772d5e4
aidanwhiteley <github@aidanwhiteley.com>
2020-01-11 17:35:33 +0000

    First draft of Travis config

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders code available text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/code available /i);
   expect(linkElement).toBeInTheDocument();
 });
