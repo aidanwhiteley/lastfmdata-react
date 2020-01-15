c1cf172c2e7022568e572e1ad212b171ab4dc38e
Aidan Whiteley <aidantemp-git@yahoo.co.uk>
2020-01-15 20:22:45 +0000

    Test fixup - after addign Redux

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 test('renders code available text', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<Provider store={store}><App /></Provider>);
   const linkElement = getByText(/code available /i);
   expect(linkElement).toBeInTheDocument();
 });
